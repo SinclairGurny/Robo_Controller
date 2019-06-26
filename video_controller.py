1f973ea7e099b167f2eeeaa8166f1b84e28bfec2
SinclairGurny <sinclairg@hotmail.com>
2019-06-26 00:13:35 -0400

    Added aruco markers back in

--- video_controller.py
+++ video_controller.py
@@ -3,8 +3,11 @@
 # June 2019: Sinclair Gurny
 
 import cv2
-import cv2.aruco
+import numpy as np
+import cv2.aruco as aruco
 import face_recognition
+import socket, threading
+import datetime, time, sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
@@ -40,6 +43,66 @@ class VThread(QThread):
             all_encodings.append(encod)
 
         return all_names, all_encodings
+
+    
+    def find_markers( self, img, mtx, dist, mem_dict ):
+        ret_img = img
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        
+        # set dictionary of arcuo markers
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+        
+        # set parameters
+        parameters = aruco.DetectorParameters_create()
+        parameters.adaptiveThreshConstant = 10
+        
+        # lists of marker ids and corners of each
+        corners, ids, rejectedPts = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+        
+        # set font for displaying
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        
+        ret_ids = []
+        
+        # check if marker is detected
+        if np.all(ids != None):
+            # estimate pose of each marker
+            rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, 0.05, mtx, dist)
+            
+            # loop over each id
+            for i in range(0, ids.size):
+                aruco.drawAxis(ret_img, mtx, dist, rvec[i], tvec[i], 0.1)
+                ret_ids.append(ids[i][0])
+                # label each marker
+                topc = corners[i][0][0]
+                topc_x, topc_y = int(topc[0]), int(topc[1])
+                cv2.putText(ret_img, 'id:'+str(ids[i][0]), (topc_x+10, topc_y+25), font, 0.4, (255,0,0), 1)
+                
+                # update memory of marker
+                t = int(time.time())
+                mem_dict[ids[i][0]] = t 
+                    
+            
+            aruco.drawDetectedMarkers(ret_img, corners)
+        
+        return ret_img, ret_ids
+    
+    def process_active_markers(self, mem_dict, curr_ids, last_ids ):
+        curr_t = int(time.time())
+        
+        for id in curr_ids:
+            if not id in last_ids:
+                print("New marker appeared:", id)
+            elif (curr_t-last_ids[id]) > 2:
+                print("Marker returned:", id)
+        
+        mem_cpy = mem_dict.copy()
+        for id in mem_cpy:
+            last_ids[id] = mem_dict[id]
+            if (curr_t-mem_dict[id]) > 2:
+                print("Marker disappeared:", id)
+                del mem_dict[id]
+        
     
     def run(self):
         self.detect = True
@@ -64,8 +127,9 @@ class VThread(QThread):
         known_names, known_encodings = self.import_faces()
             
         # Aruco marker setup
-        prev_ids = []
-        tmp_file = cv2.FileStorage('robo_cam.yaml', cv2.FILE_STORAGE_READ)
+        prev_ids = {}
+        memory = {}
+        tmp_file = cv2.FileStorage('robo_cam1.yaml', cv2.FILE_STORAGE_READ)
         cam_mat = tmp_file.getNode('camera_matrix').mat()
         dist_mat = tmp_file.getNode('dist_coeeff').mat()
         tmp_file.release()
@@ -74,6 +138,11 @@ class VThread(QThread):
             ret, frame = cap.read()
             if ret:
                 rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+                if self.do_aruco:
+                    rgbImage, ar_ids = self.find_markers( rgbImage, cam_mat, dist_mat, memory )
+                    self.process_active_markers(memory, ar_ids, prev_ids)
+
                 
                 # Face Recognition
                 if self.do_face_recog:
@@ -111,14 +180,14 @@ class VThread(QThread):
                 # Recording
                 if self.should_save_screenshot1:
                     print("Save screenshot 1!")
-                    timestamp = datetime.now().strftime('%d%b_%H:%M:%S')
+                    timestamp = datetime.datetime.now().strftime('%d%b_%H:%M:%S')
                     screenshot_name = 'Screenshots/pic_'+timestamp+'.png'
                     new_frame = frame.copy()
                     cv2.imwrite(screenshot_name, new_frame)
                     
                 if self.should_save_screenshot2:
                     print("Save screenshot 2!")
-                    timestamp = datetime.now().strftime('%d%b_%H:%M:%S')
+                    timestamp = datetime.datetime.now().strftime('%d%b_%H:%M:%S')
                     screenshot_name = 'Screenshots/pic_'+timestamp+'.png'
                     new_frame = rgbImage.copy()
                     cv2.imwrite(screenshot_name, new_frame)
